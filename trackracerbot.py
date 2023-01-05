2659cdc6d82b9cbbcb5f67ef079f2da1e1bd4ea8
Thomas Donahue <dasginganinja@gmail.com>
2023-01-04 21:04:30 -0500

    finish work for the day trying to add twitchapi for chat read/write

--- trackracerbot.py
+++ trackracerbot.py
@@ -3,6 +3,12 @@ import os
 import collections
 import threading
 
+from twitchAPI import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.types import AuthScope, ChatEvent
+from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, JoinedEvent
+import asyncio
+
 # Load the values from the .env file
 load_dotenv()
 
@@ -17,6 +23,12 @@ entry_queue = collections.deque()
 
 # Set maximum number of entries
 MAX_ENTRIES = 15
+
+# Set Twitch Channel to watch chat for
+TWITCH_CHANNEL="2BeerMinimumRacing"
+
+# Set scope for chat
+USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
 
 # Handle incoming chat messages (passed data from twitch or Youtube)
 def handle_message(message, author):
@@ -51,6 +63,8 @@ def handle_message(message, author):
     elif message.startswith("!entries"):
         # Print the queue
         print_everywhere(", ".join(entry_queue))
+    else:
+        print("I farted")
 
 # Function for printing the message in console, twitch, and youtube chats
 def print_everywhere(logmessage):
@@ -71,6 +85,7 @@ def do_test():
     handle_message("!race", "2beer")
     handle_message("!race", "AvoidRalph")
     handle_message("!join", "ArtMann")
+    handle_message("!join", "RubbingIsRacing")
     handle_message("!enter", "SuperBee2315")
     handle_message("!entries", "2beer")
     handle_message("!startrace", "ArtMann")
@@ -85,34 +100,67 @@ def do_test():
     # There should be one entry left
     handle_message("!entries", "ArtMann")
 
-# Function to listen to Twitch chat
-def listen_to_twitch():
-    # Code to connect twitch chat
+# this will be called when the event READY is triggered, which will be on bot start
+async def on_ready(ready_event: EventData):
+    print('Bot is ready for work, joining channels')
+    # join our target channel, if you want to join multiple, either call join for each individually
+    # or even better pass a list of channels as the argument
+    await ready_event.chat.join_room("2beerMinimumRacing")
+    # you can do other bot initialization things in here
+
+
+# this will be called whenever a message in a channel was send by either the bot OR another user
+async def on_message(msg: ChatMessage):
+    print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
+    # Pass this off to our handler to do queueing
+    # handle_message(msg.text, msg.user.name)
+
+async def on_joined(joined_event: JoinedEvent):
+    print("This MF joined up in some channels!")
+    print("This MF is in " + joined_event.room_name + " and my name is " + joined_event.user_name)
     
-
-    while True:
-        # Get the next message from chat
-        message = get_next_message()
-
-        # Handle the message
-        handle_message(message.message, message.author)
-
-# Function to listen to Youtube Live Chat
-def listen_to_youtube_live():
-    # Code to connect Youtube chat
-
-    while True:
-        # Get the next message from chat
-        message = get_next_message()
-
-        # Handle the message
-        handle_message(message.message, message.author)
+    await joined_event.chat.send_message(joined_event.room_name, "Locski deeeez nuts")
 
 
-# Create the chat listening threads
-thread_twitch = threading.Thread(target=listen_to_twitch)
-# thread_youtube = threading.Thread(target=listen_to_youtube_live)
+# this will be called whenever someone subscribes to a channel
+async def on_sub(sub: ChatSub):
+    print(f'New subscription in {sub.room.name}:\\n'
+          f'  Type: {sub.sub_plan}\\n'
+          f'  Message: {sub.sub_message}')
 
-# Start chat listening threads
-thread_twitch.start()
-# thread_youtube.start()
+
+# this is where we set up the bot
+async def listen_to_twitch():
+    # set up twitch api instance and add user authentication with some scopes
+    twitch = await Twitch(client_id, client_secret)
+    auth = UserAuthenticator(twitch, USER_SCOPE)
+    token, refresh_token = await auth.authenticate()
+    await twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
+
+    # create chat instance
+    chat = await Chat(twitch)
+
+    # register the handlers for the events you want
+
+    # listen to when the bot is done starting up and ready to join channels
+    chat.register_event(ChatEvent.READY, on_ready)
+    # listen for our bot joining a chat
+    chat.register_event(ChatEvent.JOINED, on_joined)
+    # listen to chat messages
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+
+    # we are done with our setup, lets start this bot up!
+    chat.start()
+
+    # lets run till we press enter in the console
+    try:
+        input('press ENTER to stop\n')
+    finally:
+        # now we can close the chat bot and the twitch api client
+        chat.stop()
+        await twitch.close()
+
+
+asyncio.run(listen_to_twitch())
+
+# do_test()
