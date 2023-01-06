6291521d9d69c2781f2c3692b91063da28cf5671
Thomas Donahue <dasginganinja@gmail.com>
2023-01-06 18:24:58 -0500

    Holy shit I got this working! We have a queue that saves to a file.

--- trackracerbot.py
+++ trackracerbot.py
@@ -8,6 +8,10 @@ import itertools
 from twitchio.ext import commands
 from twitchio.message import Message as TwitchMessage
 
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import google
+
 # Load the values from the .env file
 load_dotenv()
 
@@ -20,12 +24,26 @@ TWITCH_CHANNEL = os.getenv('TWITCH_CHANNEL')
 BOT_NAME = os.getenv('TWITCH_BOT_NAME')
 api_key = os.getenv('YOUTUBE_API_KEY')
 live_chat_id = os.getenv('YOUTUBE_LIVE_CHAT_ID')
+entry_file = os.getenv('ENTRY_FILE')
 
 # Create a queue for storing the usernames
 entry_queue = collections.deque()
 
+# Variable for absolute path to Entry File
+entry_file_abs = ""
+if entry_file is None:
+    entry_file = "entries.txt"
+
+# Set the absolute path to the savefile
+entry_file_abs = os.path.abspath(entry_file)
+
 # Set maximum number of entries
-MAX_ENTRIES = 15
+MAX_ENTRIES = 30
+
+def clear_queue():
+    # Clear the queue
+    entry_queue.clear()
+    bang_out_queue_to_file(entry_file_abs)
 
 # Handle incoming chat messages (passed data from twitch or Youtube)
 async def handle_message(message: str, author: str, twitch_message: TwitchMessage = None):
@@ -40,37 +58,54 @@ async def handle_message(message: str, author: str, twitch_message: TwitchMessag
         is_mod = twitch_message.author.is_mod
 
     if message.lower().startswith("!commands"):
-        commands_message = "Available commands: !join !entries"
+        commands_message = "Available commands: !play !entries"
         if is_mod:
-            commands_message += " // Mod Commands: !startrace !clearentries"
+            commands_message += " // Mod Commands: !start !clearentries"
         await print_everywhere(commands_message, twitch_message=twitch_message)
-    if message.lower().startswith("!race") or message.lower().startswith("!enter") or message.lower().startswith("!join") or message.count("artmannJudy"):
+
+    if message.lower().startswith("!race") or message.lower().startswith("!play") or message.lower().startswith("!enter") or message.lower().startswith("!join") or message.count("artmannJudy") or message.count("x100pr3Mychair") or message.count("x2beerShrek") or message.count("avoidr3Hotdogman") or message.count("spacec122GoodVibes") or message.count("artmannNatmar"):
         if author in entry_queue:
-            await print_everywhere("You have already entered, " + author + ". Nice try :)", twitch_message=twitch_message)
+            await print_everywhere("You have already entered " + author + ". Nice try :) artmannCheerup", twitch_message=twitch_message)
             return
 
-        # Add to entry queue
-        entry_queue.append(author)
+        # Add to queue, or print full message
+        if len(entry_queue) < MAX_ENTRIES:
+            # Add to entry queue
+            entry_queue.append(author)
 
-        # Print message when queue is full
-        if len(entry_queue) >= MAX_ENTRIES:
-            await print_everywhere("The list is full. Races starting soon!", twitch_message=twitch_message)
+            # Write to a file for the MAX_ENTRIES
+            bang_out_queue_to_file(entry_file_abs)
+            
+
+            await print_everywhere("You have been added " + author, twitch_message=twitch_message)
         else:
-            await print_everywhere("You have been added, " + author, twitch_message=twitch_message)
-    elif message.lower().startswith("!startrace") and is_mod:
-        await print_everywhere("Starting race for " + ", ".join(itertools.islice(entry_queue,0,15)), twitch_message=twitch_message)
-        # Remove the first MAX_ENTRIES from the queue
-        for i in range(MAX_ENTRIES):
-            if len(entry_queue) > 0:
-                racer = entry_queue.popleft()
+            await print_everywhere("The list is full. Better luck next race!", twitch_message=twitch_message)
+
+    elif message.lower().startswith("!start") and is_mod:
+        await print_everywhere("Starting for " + ", ".join(itertools.islice(entry_queue,0,MAX_ENTRIES)), twitch_message=twitch_message)
+        # Clear the queue
+        clear_queue()
                 
     elif message.lower().startswith("!clearentries") and is_mod:
         # Clear the queue
-        entry_queue.clear()
+        clear_queue()
         await print_everywhere("All entries have been cleared.", twitch_message=twitch_message)
+
     elif message.lower().startswith("!entries"):
         # Print the queue
         await print_everywhere("Race Entries: " + ", ".join(entry_queue), twitch_message=twitch_message)
+
+def bang_out_queue_to_file(file):
+    with open(file, 'w') as f:
+
+        for element in entry_queue:
+            # Write the element to the file, followed by a newline character
+            f.write(element + '\n')
+
+        if (len(entry_queue) < MAX_ENTRIES):
+            # Iterate over a range of numbers from len(entry_queue) to MAX_ENTRIES
+            for i in range(len(entry_queue),MAX_ENTRIES):
+                f.write('\n')
 
 # Function for printing the message in console, twitch, and youtube chats
 async def print_everywhere(logmessage: str, twitch_message: TwitchMessage = None):
@@ -131,6 +166,20 @@ class Bot(commands.Bot):
             message_text = message.content
             message_author = message.author.name
             await handle_message(message_text, message_author, twitch_message=message)
+
+# Restore the queue on restart (because we like nice things)
+if os.path.exists(entry_file_abs):
+    # Restore that shit
+    print("We restored that entry list since we restarted")
+    with open(entry_file_abs) as f:
+        # Iterate over the lines in the file
+        for line in f:
+            # Remove the newline character from the end of the line
+            line = line.strip()
+
+            if line != "":
+                # Add the line to the deque
+                entry_queue.append(line)
 
 # def listen_to_twitch():
 #     bot = Bot()
